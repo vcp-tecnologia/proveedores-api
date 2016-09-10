42048b73e9b9e6ea40f8fdd6c5c57ca689d2a53c
Rafael Ferrer <rmferrer@gmail.com>
2016-09-10 17:37:20 -0500

    scraping all category pages

--- scrapers/ingram/src/scrapers/category_scraper.js
+++ scrapers/ingram/src/scrapers/category_scraper.js
@@ -11,11 +11,14 @@ import {
 
 import {
   BASE_URL,
+  ALL_CATEGORIES_URL,
   ERROR_EXIT_CODE,
   SUCCESS_EXIT_CODE,
   RESULTS_PER_PAGE_SELECTOR,
   NEXT_PAGE_SELECTOR,
   CURRENT_PAGE_SELECTOR,
+  PRODUCT_ROW_SELECTOR,
+  CATEGORY_HEADING_SELECTOR,
   CHANGE_RESULTS_PER_PAGE_WAIT_TIME,
   PAGINATION_WAIT_TIME,
   RESULTS_PER_PAGE,
@@ -61,7 +64,7 @@ function scrapePaginatedPage(options) {
     };
   }
 
-  var rows = document.querySelectorAll('.Row');
+  var rows = document.querySelectorAll(options.productRowSelector);
   var numrows = rows.length;
   var products = [];
   var i;
@@ -88,6 +91,25 @@ function getCurrentPage(options){
   return parseInt(currentPage);
 }
 
+function scrapeCategoryUrls(options) {
+  var categoryHeaders = document.querySelectorAll(options.categoryHeadingSelector);
+  var categories = [];
+  var categoryNameRegex = /^(.*)\[.*\]$/;
+  var i;
+
+  for (i = 0; i < categoryHeaders.length; ++i) {
+    var categoryHeader = categoryHeaders[i];
+    var categoryName = categoryNameRegex.exec((categoryHeader.innerText.trim()))[1]
+    var categoryUrl = options.baseUrl + categoryHeader.children[0].getAttribute('href');
+    categories.push({
+      categoryName, 
+      categoryUrl
+    });
+  }
+
+  return categories;
+}
+
 /* END BROWSER EVALUATED FUNCTIONS */
 
 
@@ -97,69 +119,93 @@ function getCurrentPage(options){
 
 /* APPLICATION LOGIC FUNCTIONS */
 
-
-function paginateAndScrapeListings(phantom, page) {
-  let pageNumber = null;
-  paginateAndScrape();
-    
-  /* Change the page on an interval */
-  const paginationIntervalId = window.setInterval(paginateAndScrape, PAGINATION_WAIT_TIME);
-
-  function paginateAndScrape() {
-    let newPageNumber = page.evaluate(getCurrentPage, { currentPageSelector: CURRENT_PAGE_SELECTOR });
-
-    debug(`Page number is: ${newPageNumber}`);
-
-    if (newPageNumber !== pageNumber) {
-      const retVal = page.evaluate(scrapePaginatedPage, { 
-        baseUrl: BASE_URL,
-        nullValue: NULL_VALUE
-      });
-      
-      if (retVal.status === 'error') {
-        error(retVal.message);
-        logoff(phantom, page, ERROR_EXIT_CODE);
-      }
-
-      debug(`Retrieved paginated data for ${retVal.products.length} products. Page ${newPageNumber}`);
-
-      for (let i = 0; i < retVal.products.length; ++i) {
-        let product = retVal.products[i];
-        log(JSON.stringify(product), 'DATA');
-      }
-
-      pageNumber = newPageNumber;
-      page.evaluate(advanceResultsPage, { nextPageSelector: NEXT_PAGE_SELECTOR });
-    
-      info(`Waiting ${PAGINATION_WAIT_TIME / 1000} seconds for page change.`);
-    } 
-    else {
-      info(`Reached last page. Finished scraping.`); 
-      clearInterval(paginationIntervalId);
-      logoff(phantom, page, SUCCESS_EXIT_CODE);
-    }
-  }
-}
-
-function handleCategoryPage(phantom, page, categoryUrl) {
-  page.open(categoryUrl, function (status){
+function scrapeAllCategories(phantom, page) {
+  page.open(ALL_CATEGORIES_URL, function(status) {
     exitOnFailedStatus(phantom, page, status);
 
-    window.setTimeout(function() {
-      /* Change the number of results per page to minimize pagination */
-      page.evaluate(changeResultsPerPage, { 
-        resultsPerPage: RESULTS_PER_PAGE,
-        resultsPerPageSelector: RESULTS_PER_PAGE_SELECTOR
-      });
+    const categories = page.evaluate(scrapeCategoryUrls, { 
+      categoryHeadingSelector: CATEGORY_HEADING_SELECTOR,
+      baseUrl: BASE_URL 
+    });
 
-      /* Wait for successfull refresh and proceed to scrape the whole paginated subcategory */
-      window.setTimeout(paginateAndScrapeListings, CHANGE_RESULTS_PER_PAGE_WAIT_TIME, phantom, page);    
-      info(`Waiting ${CHANGE_RESULTS_PER_PAGE_WAIT_TIME / 1000} seconds for change in number of items per page.`);  
-    }, INITIAL_PAGE_LOAD_WAIT_TIME);    
-    info(`Waiting ${INITIAL_PAGE_LOAD_WAIT_TIME / 1000} seconds for page load.`);
+    let i = 0;
+
+    function nextCategory(){
+      if(i >= categories.length){
+        exit(phantom, SUCCESS_EXIT_CODE);
+      }
+      var { categoryName, categoryUrl } = categories[i];
+      info(`Starting scraping of category: ${categoryName} and url: ${categoryUrl}`);
+      handleCategoryPage(categoryUrl);
+      i++;
+    }
+
+    function handleCategoryPage(categoryUrl) {
+      page.open(categoryUrl, function (status){
+        exitOnFailedStatus(phantom, page, status);
+
+        window.setTimeout(function() {
+          /* Change the number of results per page to minimize pagination */
+          page.evaluate(changeResultsPerPage, { 
+            resultsPerPage: RESULTS_PER_PAGE,
+            resultsPerPageSelector: RESULTS_PER_PAGE_SELECTOR
+          });
+
+          /* Wait for successfull refresh and proceed to scrape the whole paginated subcategory */
+          window.setTimeout(paginateAndScrapeListings, CHANGE_RESULTS_PER_PAGE_WAIT_TIME);    
+          info(`Waiting ${CHANGE_RESULTS_PER_PAGE_WAIT_TIME / 1000} seconds for change in number of items per page.`);  
+        }, INITIAL_PAGE_LOAD_WAIT_TIME);    
+        info(`Waiting ${INITIAL_PAGE_LOAD_WAIT_TIME / 1000} seconds for page load.`);
+      });
+    }
+
+    function paginateAndScrapeListings() {
+      let pageNumber = null;
+      paginateAndScrape();
+        
+      /* Change the page on an interval */
+      const paginationIntervalId = window.setInterval(paginateAndScrape, PAGINATION_WAIT_TIME);
+
+      function paginateAndScrape() {
+        let newPageNumber = page.evaluate(getCurrentPage, { currentPageSelector: CURRENT_PAGE_SELECTOR });
+
+        debug(`Page number is: ${newPageNumber}`);
+
+        if (newPageNumber !== pageNumber) {
+          const retVal = page.evaluate(scrapePaginatedPage, { 
+            baseUrl: `${BASE_URL}/Ingrammicromx`,
+            nullValue: NULL_VALUE,
+            productRowSelector: PRODUCT_ROW_SELECTOR
+          });
+          
+          if (retVal.status === 'error') {
+            error(retVal.message);
+            logoff(phantom, page, ERROR_EXIT_CODE);
+          }
+
+          debug(`Retrieved paginated data for ${retVal.products.length} products. Page ${newPageNumber}`);
+
+          for (let i = 0; i < retVal.products.length; ++i) {
+            let product = retVal.products[i];
+            log(JSON.stringify(product), 'DATA');
+          }
+
+          pageNumber = newPageNumber;
+          page.evaluate(advanceResultsPage, { nextPageSelector: NEXT_PAGE_SELECTOR });
+        
+          info(`Waiting ${PAGINATION_WAIT_TIME / 1000} seconds for page change.`);
+        } 
+        else {
+          info(`Reached last page. Finished scraping category.`); 
+          clearInterval(paginationIntervalId);
+          nextCategory();
+        }
+      }
+    }
+
+    nextCategory();
   });
 }
-
 
 /* END APPLICATION LOGIC FUNCTIONS */
 
@@ -169,25 +215,19 @@ function parseAndValidateCmdLineArgs(phantom) {
   const system = require('system');
   const args = system.args;
 
-  if (args.length !== 3) {
-    error('Usage: phantomjs category_scraper.js [category] [url]');
+  if (args.length !== 1) {
+    error('Usage: phantomjs category_scraper.js');
     exit(phantom, ERROR_EXIT_CODE);
-  }
-  
-  return {
-    categoryName: args[1],
-    categoryUrl: args[2]
   }
 }
 
 function run(phantom) {
-  const { categoryName, categoryUrl } = parseAndValidateCmdLineArgs(phantom);
+  parseAndValidateCmdLineArgs(phantom);
 
   const page = require('webpage').create();
   configurePhantomJS(phantom, page);
 
-  info(`Starting scraping of category: ${categoryName}, and url: ${categoryUrl}`);
-  login(phantom, page, handleCategoryPage, categoryUrl);
+  login(phantom, page, scrapeAllCategories);
 }
 
 run(phantom);
