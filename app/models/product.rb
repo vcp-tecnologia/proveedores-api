11f4dcb22ee197c5453404d1f20b2e1bd199c8e7
Rafael Ferrer <rmferrer@gmail.com>
2016-09-11 00:54:03 -0500

    bugfix and parameters to controll the number of products categories to process per job

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,12 @@ class Product < ApplicationRecord
   scope :scraped_on, ->(date) { where("updated_at >= ? AND updated_at <= ?", date.to_time.beginning_of_day, date.to_time.end_of_day) }
   scope :scraped_today, -> { scraped_on(Date.today) }
   scope :missing_data, -> { where(data: nil) }
-  
+  scope :complete_data, -> { where.not(data: nil) }
+
   scope :fecha, ->(date) { where("updated_at >= ? AND updated_at <= ?", date.to_time.beginning_of_day, date.to_time.end_of_day) }
   scope :proveedor, ->(prov) { where(vendor: prov) }
   scope :categoria, ->(cat) { where(category: cat) }
-  scope :subcategoria, ->(cat) { where(subcategory: subcat) }
+  scope :subcategoria, ->(subcat) { where(subcategory: subcat) }
   scope :id_proveedor, ->(id) { where(vendor_id: id) }
   scope :fabricante, ->(fab) { where(manufacturer: fab) }
 end
