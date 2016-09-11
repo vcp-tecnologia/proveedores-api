af0ea10996724bcb82430a453195511dc45bda52
Rafael Ferrer <rmferrer@gmail.com>
2016-09-11 02:42:50 -0500

    http basic auth for products controller

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module ProveedoresApi
 
     config.scrapers_dir = Rails.root.join('scrapers')
     config.scrapers_tmp_dir = Rails.root.join('tmp', 'scrapers')
+
+    config.basic_auth_username = ENV['BASIC_AUTH_USERNAME'] || 'youzer'
+    config.basic_auth_password = ENV['BASIC_AUTH_PASSWORD'] || 'assward'
   end
 end
