32202aa8b6c5c812513a211f42f15c91b1254dbf
Rafael Ferrer <rmferrer@gmail.com>
2016-09-11 00:19:09 -0500

    revamping ingram scraper jobs

--- app/jobs/ingram_category_scraper_job.rb
+++ app/jobs/ingram_category_scraper_job.rb
@@ -1,13 +1,22 @@
 class IngramCategoryScraperJob < ApplicationJob
   queue_as :default
 
-  def perform(categoryName, categoryUrl)
+  def perform()
     baseDir = "#{Rails.application.config.scrapers_dir}/ingram"
     phantomjsBin = "#{baseDir}/node_modules/phantomjs/bin/phantomjs"
     scriptPath = "#{baseDir}/dist/category_scraper.js"
 
-    command = "#{phantomjsBin} #{scriptPath} \"#{categoryName}\" \"#{categoryUrl}\""
+    categoriesToScrape = IngramCategory.pending.limit(2)
+
+    puts "Scraping categories: #{categoriesToScrape.pluck(:name)}"
+
+    command = "#{phantomjsBin} #{scriptPath} \"#{categoriesToScrape.pluck(:url).join(' ')}\""
+
+    puts "Running command:\n\t#{command}"
+
     output = %x`#{command}`
+
+    puts "Finshed running command, with exit status #{$?.exitstatus}"
 
     if $?.exitstatus != 0
       raise ArgumentError, output
@@ -15,14 +24,33 @@ class IngramCategoryScraperJob < ApplicationJob
 
     output.split("\n").each do |line|
       if line =~ /.* \[APP DATA\] (.*)/
-        url = $1
-        pl = IngramProductListing.find_by_product_url(url)
-        if pl
-          pl.touch
+        data = $1
+        
+        puts "Parsing data: #{data}"
+
+        url = (data =~ /\"url\":\"([^\"]*)\"/) ? $1 : nil
+        sku = (data =~ /\"sku\":\"([^\"]*)\"/) ? $1 : nil
+        existencias = (data =~ /\"existencias\":\"([^\"]*)\"/) ? $1.to_i : nil
+        precio = (data =~ /\"precio\":\"([^\"]*)\"/) ? $1.to_f : nil
+        
+        
+        product = Product.find_by_url(url)
+        if product
+          puts "Found existing product, updating price: #{precio}, units: #{existencias}"
+          product.update_attributes(
+            price: precio,
+            units: existencias
+          )
         else
-          IngramProductListing.create(category: categoryName, category_url: categoryUrl, product_url: url)
-        end
+          puts "Did not find existing product. Creating one."
+          Product.create(
+            url: url,
+            price: precio,
+            units: existencias
+          )
       end
     end
+
+    categoriesToScrape.update_all(last_scraped: Time.now)
   end
 end
