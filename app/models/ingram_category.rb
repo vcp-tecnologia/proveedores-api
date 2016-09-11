3187cdf5489426a2529e484863c8e48606751f6c
Rafael Ferrer <rmferrer@gmail.com>
2016-09-10 23:09:07 -0500

    IngramCategory scopes

--- app/models/ingram_category.rb
+++ app/models/ingram_category.rb
@@ -1,2 +1,8 @@
 class IngramCategory < ApplicationRecord
+  scope :scraped_before, ->(date) { where("last_scraped < ?", date.to_time.beginning_of_day) }
+  scope :scraped_on, ->(date) { where("last_scraped >= ? AND last_scraped <= ?", date.to_time.beginning_of_day, date.to_time.end_of_day) }
+  scope :scraped_today, -> { scraped_on(Date.today) }
+  scope :pending, -> {
+    where("last_scraped IS NULL OR last_scraped < ?", Date.today.to_time.beginning_of_day)
+  }
 end
